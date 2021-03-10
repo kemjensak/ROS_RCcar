2079bd33986338c9dc670b8dbb0cd94efa5482d7
KimJinSeok <kemjensak@naver.com>
2021-03-10 16:18:50 +0900

    operating 3/10

--- IROL_RC/tamiya_jetracer/src/motion_test.py
+++ IROL_RC/tamiya_jetracer/src/motion_test.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+import rospy, geometry_msgs.msg, nav_msgs.msg
 from jetracer.nvidia_racecar import NvidiaRacecar
 import time
 car = NvidiaRacecar()
 
-car.steering = 0.9
+car.steering = 0.0
 
 print(car.steering_gain)
 print(car.steering_offset)
 
-car.throttle = 0.5
+car.throttle = -0.2
 
 print(car.throttle_gain)
 
